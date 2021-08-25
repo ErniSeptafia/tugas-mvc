c71f8601ab366d0e4addbf331b46ca84eb7e7ef2
ErniSeptafia <erniseptafiaa@gmail.com>
2021-08-25 12:30:03 +0700

    tugas

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Blog;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $blogs = Blog::all();
+        return view('blogs.index', compact('blogs'))
+        ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('blogs.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            'gambar' => 'required',
+            'judul' => 'required',
+            'content' => 'required',
+        ]);
+        Blog::create($request->all());
+        return redirect()->route('blog')
+        ->with('success','data berhasil diinput');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $blog=Blog::where('id',$id)->first();
+        return view('blogs/edit',compact('blog'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $blog=Blog::where('id',$id)->update([
+            'gambar' => $request ['gambar'],
+            'judul' => $request ['judul'],
+            'content' => $request ['content']
+        ]);
+        return redirect()->route('blog')
+        ->with('success','data berhasil diupdate');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        Blog::destroy($id);
+        return redirect ('/blog')
+        ->with('success','data berhasil dihapus');
+    }
+}
